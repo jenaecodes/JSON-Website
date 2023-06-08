d82676b16951c1a6851bfdc56c87bd7befced936
Jenae Codes <californiacollege.jennifer@gmail.com>
2023-06-08 15:19:09 -0700

    Edited JavaScript Code
    Updated JS

--- scripts/app.js
+++ scripts/app.js
@@ -3,20 +3,24 @@ const todoIdInput = document.getElementById('todoId');
 const resultDiv = document.getElementById('result');
 
 fetchButton.addEventListener('click', () => {
-    const todoId = todoIdInput.value;
-    if (todoId) {
-        const url = `https://jsonplaceholder.typicode.com/todos/${todoId}`;
-
+  const todoId = todoIdInput.value;
+  if (todoId) {
+    const url = `https://jsonplaceholder.typicode.com/todos/${todoId}`;
 
     fetch(url)
-          .then(response => response.json())
-          .then(data => {
-            resultDiv.innerHTML = JSON.stringify(data, null, 2);
-          })
-          .catch(error => {
-            console.error(error);
-          });
-        } else {
-            resultDiv.innerHTML - 'cringe';
-        }
-    });
+      .then(response => response.json())
+      .then(data => {
+        resultDiv.innerHTML = `
+        <p>User ID: ${data.userId}</p>
+        <p>ID: ${data.id}</p>
+        <p>Title: ${data.title}</p>
+        <p>Completed: ${data.completed}</p>
+      `;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  } else {
+    resultDiv.innerHTML = 'Please enter a ToDo ID';
+  }
+});
